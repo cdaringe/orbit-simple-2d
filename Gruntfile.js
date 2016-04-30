e1383bb310c558fff28a1a3daa8049a19d723368
cdaringe <cdaringe@gmail.com>
2016-04-30 13:07:35 -0600

    standard-gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,32 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-browserify')
+  grunt.loadNpmTasks('grunt-mocha')
 
-    grunt.initConfig({
-        browserify: {
-            dist: {
-                src: './src/orbit-simple-2d.js',
-                dest: 'build/orbit-simple-2d.js',
-                options: {
-                    keepalive: true,
-                    watch: true
-                },
-            },
-            ex: {
-                src: './src/orbit-example.js',
-                dest: 'build/orbit-example.js'
-            }
+  grunt.initConfig({
+    browserify: {
+      dist: {
+        src: './src/orbit-simple-2d.js',
+        dest: 'build/orbit-simple-2d.js',
+        options: {
+          keepalive: true,
+          watch: true
         },
-        mocha: {
-            test: {
-                src: ['test/**/*.html'],
-            },
-            options: {
-                run: true
-            }
-        }
-    });
+      },
+      ex: {
+        src: './src/orbit-example.js',
+        dest: 'build/orbit-example.js'
+      }
+    },
+    mocha: {
+      test: {
+        src: ['test/**/*.html'],
+      },
+      options: {
+        run: true
+      }
+    }
+  })
 
-    grunt.registerTask('test', ['mocha']);
-    grunt.registerTask('default', ['browserify']);
-};
+  grunt.registerTask('test', ['mocha'])
+  grunt.registerTask('default', ['browserify'])
+}
